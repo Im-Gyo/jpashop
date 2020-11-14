8b869d08aefe250e2be868df6b0ff7b3d95e0d31
Im-Gyo <jywlsh3@naver.com>
2020-11-14 23:47:35 +0900

    홈화면, 레이아웃

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -21,5 +22,6 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-    // public List<Order> findAll(OrderSearch orderSearch){}
+    /* public List<Order> findAll(OrderSearch orderSearch){
+     }*/
 }
